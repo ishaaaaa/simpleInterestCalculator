ea56859209783c85bc3d6ed0184e54c5426378a6
Isha <ishingote@gmail.com>
2021-12-01 12:23:10 +0000

    Simple Interest Calculator

---
+++ sicalculator.c
@@ -0,0 +1,27 @@
+#include<stdio.h>  
+
+int main()  
+
+    {  
+
+        float PA , Rate , Time , SI ;  //Principle amount, Rate of interest, Duration in months, Simple interest
+
+        printf("Enter the Principle Amount");
+
+        scanf("%f\n", &PA);
+
+        printf("Enter the Rate of Interest");
+
+        scanf("%f\n", &Rate);
+
+        printf("Enter the Duration in months");
+
+        scanf("%f\n", &Time);
+
+        SI  = (PA*Rate*Time)/100;  
+
+        printf("\n\n Simple Interest is : %f", SI);  
+
+        return (0);  
+
+    }  
